9d129332e2a26fcecc437d43bc6697fd536cf45a
daniu88 <380494351@qq.com>
2017-07-07 13:59:56 +0800

    99

---
+++ 7-7/zuoye.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>九九乘法表 </title>
+</head>
+<body>
+	<?php 
+	echo "<table border=1>";
+	for ($i = 9; $i >= 1; $i--) { 
+		echo "<tr>";
+			for ($y=1; $y <= $i ; $y++) { 
+				echo "<td>";
+				echo "88888";
+				echo "</td>";
+			}
+		echo "<tr>";
+	}
+
+	echo "<table>";
+	 ?>
+</body>
+</html>
